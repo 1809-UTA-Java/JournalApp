4bbca907548ad579b05b05385eeedd0b245e6e2c
selsuean <selsuean@gmail.com>
2018-09-27 10:21:09 -0500

    better but still broken

--- src/com/revature/JournalApp.java
+++ src/com/revature/JournalApp.java
@@ -18,10 +18,66 @@ public class JournalApp {
 	public static void main(String[] args) {
 		String filename = "journal entry";
 
+		Scanner scan = new Scanner(System.in);
+
+		String anEntry = makeAnEntry(scan);
+		
+
+
+		System.out.println("Do you want to save this entry? Type y/n.");
+		String saveAns = scan.next();
+
+		if (saveAns.equals("y") || saveAns.equals("Y")) {
+			writeReadFile(filename,anEntry);
+			System.out.println("Entry has been saved to " + filename + ". Thank you.");
+		}
+		if (saveAns.equals("n") || saveAns.equals("N")) {
+			System.out.println("Fine. Entry will be discarded. Goodbye.");
+		}
+		else {
+			System.out.println("I do not understand. Goodbye.");
+		}
+		scan.close();
+		
+
+		
 
 
 
-		Scanner sc = new Scanner(System.in);
+		// Scanner sc = new Scanner(System.in);
+
+		// System.out.println("How was your day today?");
+
+		// String entry = sc.nextLine();	
+		// System.out.println(entry);
+
+
+		// sc.close();
+
+
+
+		// try { FileWriter fw = new FileWriter(filename);
+		// fw.write(entry);
+		// fw.close();
+		// } catch(IOException io) {
+		// 	io.printStackTrace();
+		// }
+
+		// try { FileReader fr = new FileReader(filename);
+		// 	int i;
+		// 	while ((i = fr.read()) != -1) {
+		// 		System.out.println((char) i);
+		// 	}} catch(FileNotFoundException ff) {
+		// 		ff.printStackTrace();
+		// 	} catch(IOException io2) {
+		// 		io2.printStackTrace();
+		// 	} 
+
+		
+	}
+
+	public static String makeAnEntry(Scanner sc) {
+		//Scanner sc = new Scanner(System.in);
 
 		System.out.println("How was your day today?");
 
@@ -29,28 +85,28 @@ public class JournalApp {
 		System.out.println(entry);
 
 
-		sc.close();
+		//sc.close();
+		return entry;
+	}
 
-
-
-		try { FileWriter fw = new FileWriter(filename);
-		fw.write(entry);
-		fw.close();
-		} catch(IOException io) {
-			io.printStackTrace();
-		}
-
-		try { FileReader fr = new FileReader(filename);
-			int i;
-			while ((i = fr.read()) != -1) {
-				System.out.println((char) i);
-			}} catch(FileNotFoundException ff) {
-				ff.printStackTrace();
-			} catch(IOException io2) {
-				io2.printStackTrace();
-			} 
-
-		
+	public static void writeReadFile(String myFile, String myEntry) {
+		try { FileWriter fw = new FileWriter(myFile);
+			fw.write(myEntry);
+			fw.close();
+			} catch(IOException io) {
+				io.printStackTrace();
+			}
+	
+			try { FileReader fr = new FileReader(myFile);
+				int i;
+				while ((i = fr.read()) != -1) {
+					System.out.println((char) i);
+				}
+				fr.close(); } catch(FileNotFoundException ff) {
+					ff.printStackTrace();
+				} catch(IOException io2) {
+					io2.printStackTrace();
+				} 
 	}
 
 }
