0e61c23d2ba5b8b9aa599b7c5e0198ab6830dd25
Zach Leonard <zachariah.s.leonard@gmail.com>
2018-09-28 11:03:48 -0500

    Menu and general inflation

--- src/com/revature/journal/JournalApp.java
+++ src/com/revature/journal/JournalApp.java
@@ -8,58 +8,101 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
-*
+* FileIO and ArrayList demo
+* JournalApp
+* Creates and/or opens a text file and saves user input
+* @authore Zach Leonard
 */
 class JournalApp {	
 	public static void main (String[] args) {
+		ClearTerminal();
+		ArrayList<String> appendices = new ArrayList<>(); //Am I polluting the string pool?
 		String fileName;
 		String userAppendInput = " ";
-		do {
-			fileName = GetFileNameFromInput();
-		} while (!IsValidFileName(fileName));
-		try {
-        		FileWriter fw = new FileWriter(fileName);
-			System.out.println("Program will append to file line by line. To exit appending, type the sentinel: !~");
-			while (!userAppendInput.equals("!~")) {
-				userAppendInput = GetInputToAppendToFile();
-				AppendToFile(fw, userAppendInput);
-			}
-			fw.close(); 
-        		
-        	} catch (IOException ex) {
-        		ex.printStackTrace();
-        	}
+		int menuSelection = Menu();
+		switch (menuSelection) {
+			case 1:
+				do {
+					fileName = GetFileNameFromInput();
+				} while (!IsValidFileName(fileName));
+				try {
+        				FileWriter fw = new FileWriter(fileName, true);
+					System.out.println("Program will append to file line by line. To exit appending, type the sentinel: !~");
+					while (!userAppendInput.equals("!~")) {
+						userAppendInput = GetInput();
+						appendices.add(userAppendInput);				
+					}
+					//Commit appendices to file
+					AppendToFile(fw, appendices);
+					fw.close(); 	
+        			} catch (IOException ex) {
+        				ex.printStackTrace();
+					System.out.println("Not able to create/open the specified file!");
+        			}	
+				break;
+			case 2:
 		
-		
+				break;
+			case 3: 
+				break;
+		}		
 	}
 	static String GetFileNameFromInput() {
 		String fileName;		
 		Scanner sc = new Scanner(System.in);
-        	System.out.println("What is your journal file name to create and/or open? (one word input only)");
+        	System.out.println("What is your journal file name to create and/or open? (No spaces)");
         	fileName = sc.nextLine();
-        	System.out.println(fileName);
-		return fileName;
+		ClearTerminal();
+        	return fileName;
 	}
 	static boolean IsValidFileName (String fileName) {
 
-		//implement
-		return true;
+		Pattern pattern = Pattern.compile("\\s");
+		Matcher matcher = pattern.matcher(fileName);
+		return (!matcher.find());
 
 	}
-	static void AppendToFile(FileWriter fw, String stringToAppend) {
+	static void AppendToFile(FileWriter fw, ArrayList<String> appendices) {
 		try {
-			if (!stringToAppend.equals("!~")) {
-				fw.append(stringToAppend + "\n");        	
-			}
+			for (int i = 0; i < appendices.size(); i++) {		      
+           			if (!appendices.get(i).equals("!~")) {
+					fw.append(appendices.get(i) + "\n");        	
+				}	
+      			}
 		} catch (IOException ex) {
             		ex.printStackTrace();
         	}
+		ClearTerminal();
 	}
-	static String GetInputToAppendToFile() {
+	static String GetInput() {
 		Scanner sc = new Scanner(System.in);
 		return sc.nextLine();
 	}
-	
+	public static void ClearTerminal() {  
+    		System.out.print("\033[H\033[2J");  
+    		System.out.flush();  
+	}
+	//A rather unfortunate text menu
+	public static int Menu() {
+		int selection = 0;
+		while (!((selection == 1) || (selection == 2) || (selection == 3))) {
+			ClearTerminal();
+			System.out.println("1. Write in journal");
+			System.out.println("2. Read journal");
+			System.out.println("3. Exit");
+			System.out.print("Enter menu selection: ");
+			try {
+				selection = Integer.parseInt(GetInput());
+			} catch (Exception ex) {
+				selection = 0;
+			}
+		}
+		ClearTerminal();
+		return selection;
+	}  	
 }
