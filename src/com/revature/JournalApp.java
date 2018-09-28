324f62a6efc3526ee3d6fecd2befced414132536
Jcewazhere <jcewazhere@yahoo.com>
2018-09-28 11:06:18 -0500

    WIP

--- src/com/revature/JournalApp.java
+++ src/com/revature/JournalApp.java
@@ -8,25 +8,30 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 import java.io.InputStream;
+//is one import java.io.* better than  seperate import lines?
+import java.util.Scanner;
 import java.util.BufferedInputStream;
 
-class journalApp{
+public class journalApp{
 public static void main (String args[]) {
     String fileName = "Journal";
     Scanner sc = new Scanner(System.in);
 
     system.out.println("What do you want to write in your journal today?");
     system.out.println("Enter EXIT to stop writing.");
-    while(true){try {String inputText = sc.nextLine();
-                try(FileWriter fw = new FileWriter(fileName)){
+    try {
+        while(true){
+            String inputText = sc.nextLine();
+                try{
+                    FileWriter fw = new FileWriter(fileName);{
                     if(sc.nextLine.equal("EXIT")){break;}
                     fw.append(inputText);
                     sytem.out.println("You added " +inputText +" to your journal.");
-                }catch(IOException ex){ex.printStackTrace();}
-            }catch (Exception e){ex.printStackTrace();}
-            finally {sc.close();}
+                    }catch(IOException ex){ex.printStackTrace();}
+                }catch (Exception e){
+                    ex.printStackTrace();
+                    }finally {sc.close();}
+        }
     }
-}
 }
