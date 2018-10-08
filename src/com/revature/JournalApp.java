83a10ee8de36421622c2accf8102b2b7f1789262
nshresth7 <nishan_shrestha11@yahoo.com>
2018-10-08 00:32:34 -0500

    study commit

--- src/com/revature/JournalApp.java
+++ src/com/revature/JournalApp.java
@@ -1,34 +1,37 @@
 package com.revature;
 
-import java.util.Scanner;
+import java.util.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-
+/*
+ * @Nishan Shrestha
+ * studying code
+ */
 public class JournalApp{
 
     public static void main(String[] args){
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Name your file...");
-        String fileName = scan.nextLine();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        String fileName = input.nextLine();
 
         try{
-            FileWriter fw = new FileWriter(fileName);
-            System.out.println("What would you like to write to " + fileName);
-            String user = scan.nextLine();
-            fw.write(user);
-            fw.close(); 
+            FileWriter fwrite = new FileWriter(fileName);
+            System.out.println("Write your journal " + fileName);
+            String user = input.nextLine();
+            fwrite.write(user);
+            fwrite.close(); 
         } 
         catch (IOException ex) {
              ex.printStackTrace();
         }
 
         System.out.println("Reading file " + fileName);
-        try (FileReader fr = new FileReader(fileName)) {
+        try (FileReader fread = new FileReader(fileName)) {
             int i;
-            while ((i = fr.read()) != -1) {
+            while ((i = fread.read()) != -1) {
                 System.out.print((char) i);
             }
         } 
